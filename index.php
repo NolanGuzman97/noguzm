55ef84f8d76abcb5b4716718bc7ec69d1398f4a1
Nolan <noguzman@csumb.edu>
2017-08-25 20:29:36 +0000

    adding index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+
+
+        <h1>
+            Welocome to Nolan's Page
+        </h1>
+    </body>
+</html>
